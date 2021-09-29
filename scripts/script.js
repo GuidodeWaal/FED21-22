61d9162db9ede86de4cc1b1b6e19670fcdfd9b96
GuidodeWaal <90271373+GuidodeWaal@users.noreply.github.com>
2021-09-29 14:57:36 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,22 @@ hamburger =
 hamburger.addEventListener("click", uitklap);
 
 function uitklap (){
-  let backdrop = 
-      document.querySelector("main>div")
-  backdrop.classList.add("anders");
+    let backdrop = 
+        document.querySelector(".backdrop")
+    backdrop.classList.toggle("anders");
+
+    let menu = 
+        document.querySelector(".menu")
+    menu.classList.toggle("anders");
+}
+
+var footerbutton
+footerbutton =
+    document.querySelector("footer>button");
+footerbutton.addEventListener("click", uitklap2)
+
+function uitklap2 () {
+    let button = 
+        document.querySelector ("footer>button")
+    button.classList.toggle("anders");
 }
